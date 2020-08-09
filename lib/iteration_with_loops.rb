94cd6b1684587b7e0e24b4c9bdaa6ac34f167951
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-08-09 02:30:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,14 +5,13 @@ def find_min_in_nested_arrays(src)
     temp_index = 0
     min_value = 100
     while temp_index < src[row_index].count do
-      min_value = 100
       if src[row_index][temp_index] < min_value
         min_value = src[row_index][temp_index]
+        end
         temp_index += 1
-      end
-       min_temps << min_value
+     end
+     min_temps << min_value
       row_index += 1
     end
     min_temps
   end
-end
